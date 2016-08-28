ea134200df172d81caddf50fcd0acc780d6ab7f5
Dersim Davaod <dersim.davaod@hiqo-solutions.com>
2016-08-28 20:44:58 +0300

    BluetoothHarwareControlManager. Put BluetoothHarwareControlObserver into Xcode project.

---
+++ lib/ios/ThaliCore/ThaliCoreCITests/Utils/HardwareControl/Bluetooth/BluetoothHarwareControlObserver.h
@@ -0,0 +1,13 @@
+//
+//  BluetoothHarwareControlObserver.h
+//  ThaliCore
+//
+//  Created by Dersim Davaod on 8/18/16.
+//  Copyright © 2016 Thali. All rights reserved.
+//
+
+#ifndef BluetoothHarwareControlObserver_h
+#define BluetoothHarwareControlObserver_h
+
+
+#endif /* BluetoothHarwareControlObserver_h */
